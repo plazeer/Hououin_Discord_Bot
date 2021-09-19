cddab9d989795cce8c325323769d2ac0ff4715f6
Tom <toumj91@gmail.com>
2021-09-19 17:37:10 +0200

    ftg update

--- commands/ftg.js
+++ commands/ftg.js
@@ -2,17 +2,15 @@ exports.run = (client, message, args) => {
     const ms = require('ms');
     const cooldown = new Set();
     member = message.author.id;
-if (cooldown.has(member)) {
-    message.channel.send("spamo no")
-
-} else {
-       // NolanID = message.guild.members.fetch('286463065824952322');
-        message.channel.send('<@'+member+'> ftg');
-        cooldown.add(member);
-        setTimeout(() => {
-        cooldown.delete(member);
-        }, 18000);
-    }
-    
- 
+if (cooldown.has(message.author.id)) {
+    message.channel.send("spamo no");
 }
+else {
+    message.channel.send('<@'+member+'> ftg');
+    };
+        // NolanID = message.guild.members.fetch('286463065824952322');
+        cooldown.add(message.author.id);
+        setTimeout(() => {
+        cooldown.delete(message.author.id);
+        }, 180000);
+};
