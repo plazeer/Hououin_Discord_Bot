d0f2de1eef1b38e67fdb3d192e6e2ef778d2a5f6
Tom <toumj91@gmail.com>
2021-12-03 19:14:45 +0100

    help command

---
+++ commands/clear.js
@@ -0,0 +1,9 @@
+exports.run = async (client, message, args) => {
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You don't have permission to use this command.");
+    if (!args[0]) return message.channel.send('pas de nombre');
+    var nombre = args[0];
+    var n = parseInt(nombre)+1;
+    message.delete();
+    message.channel.bulkDelete(n, true)
+    message.channel.send(nombre+" messages supprimés");
+}
