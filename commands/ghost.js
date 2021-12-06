5490653e20e3627e94ed1db2b5bff04bd902dd10
Tom <toumj91@gmail.com>
2021-12-06 17:11:35 +0100

    ghost update

--- commands/ghost.js
+++ commands/ghost.js
@@ -1,16 +1,11 @@
 const cooldown = new Set();
 exports.run = (client, message, args) => {
-    const ms = require('ms');
     const member = message.author.id;
     const list = client.guilds.cache.get("584833650193858814");
-    list.members.fetch()
-    const M = list.members.cache.map(membre => membre.id);
-    console.log(M);
-    
+    const M = list.members.cache.map(membre => membre.id);    
     max = M.length;
     random = Math.floor(Math.random() * max);
-    plusone = random+1;
-    let picked = M.slice(random,plusone);
+    let picked = M.slice(random,random+1);
     
     if (cooldown.has(member)) {
         message.channel.send("attend bozo");
