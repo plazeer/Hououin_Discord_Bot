2da8033a25347290c406361c40fd03ce46e04d67
Tom <toumj91@gmail.com>
2021-12-06 21:13:40 +0100

    update ghost

--- commands/ghost.js
+++ commands/ghost.js
@@ -2,6 +2,7 @@ const cooldown = new Set();
 exports.run = (client, message, args) => {
     const member = message.author.id;
     const list = client.guilds.cache.get("584833650193858814");
+    list.members.fetch()
     const M = list.members.cache.map(membre => membre.id);    
     max = M.length;
     random = Math.floor(Math.random() * max);
