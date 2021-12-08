2cffebd7175223e12bbf1f346401b3d4f2f9239c
Tom <toumj91@gmail.com>
2021-12-08 20:48:00 +0100

    say

--- commands/say.js
+++ commands/say.js
@@ -1,4 +1,5 @@
 exports.run = async (client, message, args) => {
+    //if (message.author.id == 286463065824952322) return message.channel.send("ftg nolan connard")
     let phrase = args.join(" ");
     message.delete();
     await message.channel.send(phrase);
