088f7b1cb456b5863625879544b7b05f5cf7806d
Tom <toumj91@gmail.com>
2022-05-20 17:21:12 +0200

    leaderboard

---
+++ DBTable.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('tags', {
+    	name: {
+    		type: DataTypes.STRING,
+    		unique: true,
+    	},
+        userid: {
+    		type: DataTypes.STRING,
+    		unique: true,
+    	},
+    	Wins: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        Loses: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        One: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        Two: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        Three: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        Four: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        Five: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+        Six: {
+            type: DataTypes.INTEGER,
+    		defaultValue: 0,
+    		allowNull: false,
+        },
+    });
+}
