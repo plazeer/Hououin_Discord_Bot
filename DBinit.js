5ca000c54e87fea30465b92e2d3e61ea7c430de6
Tom <toumj91@gmail.com>
2022-05-21 16:48:49 +0200

    database wordle

--- DBinit.js
+++ DBinit.js
@@ -1,12 +1,16 @@
-const Sequelize = require('sequelize');
-
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize('database', 'username', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
 	logging: false,
 	storage: 'database.sqlite',
 });
+const queryInterface = sequelize.getQueryInterface();
+
 require('./DBTable.js')(sequelize, Sequelize.DataTypes);
+//queryInterface.removeColumn('tags', 'Played', { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false});
+queryInterface.addColumn('tags', 'Played', { type: DataTypes.INTEGER, defaultValue: 0, allowNull: false});
+
 
 const force = process.argv.includes('--force') || process.argv.includes('-f');
 
