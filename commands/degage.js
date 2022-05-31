51f8f8b1eed980ca653600d0b537c2fa3c014129
Tom <toumj91@gmail.com>
2022-05-31 12:40:31 +0200

    + degage

---
+++ commands/degage.js
@@ -0,0 +1,31 @@
+const { disco } = require("../functions/punition_roulette");
+
+const cooldown = new Set();
+module.exports = {
+    name: 'degage',
+    aliases: ['dg'],
+    cooldown: 0,
+    description: 'motus/sutom/wordle',
+    async run(client, message, args, cmd){
+        let list = [];
+        const member = message.author.id;
+        const guild = client.guilds.cache.get(message.guild.id);
+        guild.members.fetch().then((members) => {});
+        let username = await guild.members.fetch(message.author.id)
+        await message.guild.channels.cache.get(username.voice.channel.id).members.forEach((member) => {
+            if (member.id === message.author.id) return;
+            list.push(member);
+        });
+        if (!list[0]) return message.channel.send("Faut qu'il y ai des gens en vocal")
+        disco(list[Math.floor(Math.random() * list.length)])
+        setTimeout(() => {
+            cooldown.delete(member);
+        }, 86400000);
+    }
+}
+
+exports.disco = async function(user){
+    console.log("déconnecter ============================");
+    console.log(user.user.username+" s'est fait déco")
+    user.voice.disconnect();  //déco du vc
+};
